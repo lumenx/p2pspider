9c8c6bdfddf925fcce992ba1ca637365462fef5c
IceManNCSU <admin@redsox.cc>
2016-07-26 13:41:35 -0400

    Changes to use ElasticSearch

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 'use strict';
 
+var elasticsearch = require('elasticsearch');
+var moment = require('moment');
 var P2PSpider = require('./lib');
-var models = require('./models');
+//var models = require('./models');
+
+var es = new elasticsearch.Client({
+    host: 'home.redsox.cc:9383',
+    log: 'info'
+});
 
 var p2p = P2PSpider({
     nodesMaxSize: 100,   // be careful
@@ -10,70 +17,87 @@ var p2p = P2PSpider({
 });
 
 p2p.ignore(function (infohash, rinfo, callback) {
-    // false => always to download the metadata even though the metadata is exists.
-    var theInfohashIsExistsInDatabase = false;
-    callback(theInfohashIsExistsInDatabase);
+
+    es.get({
+        index: index,
+        type: type,
+        id: infohash
+    })
+    /*.then(function(res) {
+        return es.update({
+            index: index,
+            type: type,
+            id: infohash,
+            body: {
+                script: 'ctx._source.peer_counter += peer',
+                params: {
+                    peer: 1
+                }
+            }
+        });
+    })*/
+    .then(function(res) {
+        //console.log('infohash exist:%s', infohash);
+        console.log('OLD - %s, infohash);
+    })
+    .catch(function(err, res) {
+        if (err.status && err.status == 404) {
+            callback(false);
+        } else {
+            console.err(err);
+            process.exit(1);
+        }
+    });
+
 });
 
+var index = 'test2';
+var type = 'seed';
+
 p2p.on('metadata', function (metadata) {
-    
-    metadata.infohash;
-    metadata.magnet;
-    metadata.port;
-    metadata.address;
-    
-    
-    //array
-    metadata.info.files;
-    metadata.info.name;
-    metadata.info['piece length'];
-    metadata.info.pieces;
+
+    //console.log('=======================================');
+    //console.log('hash:%s', metadata.infohash);
+    //console.log('name:%s', metadata.info.name.toString());
 
     var files = [];
+    var totalLength = 0;
 
-    var totalSize = 0;
-    
-    try {
-    if (metadata.info.files) {
-        metadata.info.files.forEach(function(file) {
-            file.path.forEach(function(path) {
-                files.push(path.toString());
-            });
-            totalSize = totalSize + file.length;
-        });
-    }else {
-       totalSize = totalSize + metadata.info.length;
-    }
+    try{
+       if (metadata.info.files) {
+           metadata.info.files.forEach(function(file) {
+               file.path = file.path.toString();
+               files.push({length: file.length, path: file.path});
+               totalLength += file.length;
+           });
+       } else {
+           totalLength = metadata.info.length;
+           files.push({length: metadata.info.length, path: metadata.info.name.toString()});
+       }
+
+       es.create({
+           index: index,
+           type: type,
+           id: metadata.infohash,
+           body: {
+               name: metadata.info.name.toString(),
+               files: JSON.stringify(files),
+               length: totalLength,
+               peer_counter: 1,
+               created_at: (new Date())
+           }
+       })
+       .then(function(res) {
+          console.log('NEW - %s - $s', metadata.infohash, metadata.info.name.toString());
+       })
+       .catch(function(err) {
+           console.log(err);
+       });
     }
     catch(err){
        console.log(err);
-    }                
-
-    //Make sure you add the UNIQUE INDEX to the 'hash' column
-    //This will keep laggy MySQL from allowing duplicate HashID's from multiple crawlers
-
-    //ALTER IGNORE TABLE `Magnets` ADD UNIQUE INDEX (`hash`);
-
-    models.Magnet.findOne({where: {hash: metadata.infohash}})
-    .then(function(magnet) {
-        if (magnet) {
-            console.log('OLD - %s - %s', metadata.infohash, magnet.name);
-            return magnet.increment('node_count', {by: 1});
-        }
-        console.log('NEW - %s - %s', metadata.infohash, metadata.info.name.toString());
-        return models.Magnet.create({
-            hash: metadata.infohash,
-            name: metadata.info.name.toString(),
-            files: JSON.stringify(files),
-            size: totalSize,
-            node_count: 1
-        });
-    })
-    .catch(function(error) {
-        console.log(error);
-    });
+    }
+    
 });
 
-models.sequelize.sync().then(function () {
-    p2p.listen(6881, '0.0.0.0');
-});
+p2p.listen(6881, '0.0.0.0');
